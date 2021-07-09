13a4d17f3b757c3b1f4ce03586dcb733a875d442
Takahiro-Koyama <87005124+Takahiro-Koyama@users.noreply.github.com>
2021-07-09 11:05:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('hogehogehoge')
+        self.response.write('hogehogehoge!!')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
